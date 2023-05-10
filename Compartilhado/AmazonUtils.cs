a5a8651380338f7b2cad54fd7450e8036a0575dd
Saulo <saulomas@gmail.com>
2023-05-10 12:42:48 -0300

    Add project files.

---
+++ Compartilhado/AmazonUtils.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Compartilhado.Model;
+
+namespace Compartilhado
+{
+    public static class AmazonUtils
+    {
+        public static async Task SalvarAsync(this Pedido pedido)
+        {
+            var client = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+            var context = new DynamoDBContext(client);
+            await context.SaveAsync(pedido);
+        }
+    }
+}
---
+++ Compartilhado/Model/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Compartilhado.Model
+{
+    [DynamoDBTable("pedidos")]
+    public class Pedido
+    {
+        public string Id { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public DateTime DataDeCriacao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public Pagamento Pagamento { get; set; }
+
+        public string Justificativa { get; set; }
+
+        public string Status { get; set; }
+
+        public bool Pago { get; set; }
+
+        public bool Faturado { get; set; }
+    }
+}
